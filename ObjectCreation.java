9e8a929fc24fa058a54ce2a6c2bb8a066b11d745
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-09-15 09:08:24 +0530

    Add files via upload

--- ObjectCreation.java
+++ ObjectCreation.java
@@ -1,9 +1,53 @@
 package sample;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ObjectCreation {
+	
+	
+	public static List<ProductInfo> generateProductInfo(){
+		List<Specification> specifications = new ArrayList<Specification>();
+ 		
+		Specification specification = new Specification(4, 64);
+		Specification specification2 = new Specification(6, 128);
+		specifications.add(specification);
+		specifications.add(specification2);
+		
+		ProductInfo info = new ProductInfo(1, "Samsung", 20000, specifications);
+		System.out.println(info);
+		
+		List<Specification> specifications1 = new ArrayList<Specification>();
+ 		
+		Specification specification3 = new Specification(6, 64);
+		Specification specification4 = new Specification(7, 128);
+		specifications1.add(specification3);
+		specifications1.add(specification4);
+		ProductInfo info2 = new ProductInfo(2, "Nothing 2a", 40000, specifications1);
+		
+		List<ProductInfo> infos = new ArrayList<ProductInfo>();
+		infos.add(info);
+		infos.add(info2);
+		return infos;
+	}
+	
+	
+	public static Set<ProductInfo> filterProduct(List<ProductInfo> products){
+		
+		Set<ProductInfo> result = new HashSet<ProductInfo>();
+		for(ProductInfo product: products) {
+			for(Specification spec: product.getSpecification()) {
+				if(spec.getRAM() > 6) {
+					result.add(product);
+				}
+			}
+			
+		}
+		return result;
+	}
+	
 
 	public static void main(String[] args) {
 		EmployeeDetails emp1 = new EmployeeDetails();
@@ -19,24 +63,14 @@ public class ObjectCreation {
 		System.out.println(emp1);
 		System.out.println(emp2);
 		
-//		ProductInfo info = new ProductInfo(1, "Vivo Mobiles", 20000);
-//		System.out.println(info.getProductName());
-//		System.out.println(info);
-//
-//		ProductInfo info2 = new ProductInfo();
-//		info2.setId(2);
-//		info2.setPrice(20000);
-//		info2.setProductName("Samsung");
 		
-		List<Specification> specifications = new ArrayList<Specification>();
- 		
-		Specification specification = new Specification(4, 64);
-		Specification specification2 = new Specification(6, 128);
-		specifications.add(specification);
-		specifications.add(specification2);
+		List<ProductInfo> products= generateProductInfo();
 		
-		ProductInfo info = new ProductInfo(1, "Samsung", 20000, specifications);
-		System.out.println(info);
+		Set<ProductInfo> result = filterProduct(products);
+		System.out.println("The filtered products are"+ result);
+		
+		
+		
 		
 	}
 	
